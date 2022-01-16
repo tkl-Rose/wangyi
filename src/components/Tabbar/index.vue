15130d4b92371e9cc0160f47bb39d728eba52a38
余笙 <7693836+qingfenga@user.noreply.gitee.com>
2022-01-16 21:35:47 +0800

    ti

--- src/components/Tabbar/index.vue
+++ src/components/Tabbar/index.vue
@@ -1,12 +1,28 @@
 <template>
    
-  <van-tabbar active-color="#ee0a24" v-model="active">
-        <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>    
-    <van-tabbar-item icon="apps-o">分类</van-tabbar-item>    
-    <van-tabbar-item icon="smile-comment-o">值得买</van-tabbar-item>    
-    <van-tabbar-item icon="cart-o">购物车</van-tabbar-item>    
-    <van-tabbar-item icon="user-o">个人</van-tabbar-item>  
+  <van-tabbar route active-color="#ee0a24">
+           
+    <van-tabbar-item replace to="/home" icon="wap-home-o">首页</van-tabbar-item>
+           
+    <van-tabbar-item replace to="/category" icon="apps-o">分类</van-tabbar-item>
+           
+    <van-tabbar-item replace to="/worth-buy" icon="smile-comment-o"
+      >值得买</van-tabbar-item
+    >
+           
+    <van-tabbar-item replace to="/cart" icon="cart-o">购物车</van-tabbar-item>  
+         
+    <van-tabbar-item replace to="/center" icon="user-o">个人</van-tabbar-item>  
+     
   </van-tabbar>
+   
+  <!-- <van-tabbar active-color="#ee0a24" v-model="active">
+    <van-tabbar-item name="home" icon="wap-home-o">首页</van-tabbar-item>    
+    <van-tabbar-item name="category" icon="apps-o">分类</van-tabbar-item>    
+    <van-tabbar-item name="worth-buy" icon="smile-comment-o">值得买</van-tabbar-item>
+    <van-tabbar-item name="cart" icon="cart-o">购物车</van-tabbar-item>    
+    <van-tabbar-item name="center" icon="user-o">个人</van-tabbar-item>  
+  </van-tabbar> -->
 </template>
 
 <script>
